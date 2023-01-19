8e1ff91972a54f27a62a8e23d77d21a9bba6aa75
zichouyin <81036790+zichouyin@users.noreply.github.com>
2023-01-19 21:50:36 +0800

    Update TermuxAlpine.sh

--- TermuxAlpine.sh
+++ TermuxAlpine.sh
@@ -47,7 +47,7 @@ checksysinfo() {
 	printf "$blue [*] Checking host architecture ..."
 	case $(getprop ro.product.cpu.abi) in
 		arm64-v8a)
-			SETARCH=aarch64
+			SETARCH=armhf
 			;;
 		armeabi|armeabi-v7a)
 			SETARCH=armhf
